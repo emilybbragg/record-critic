96db736bd1b9a097c1f75aca1afb1e232440c65f
Emily <emilybribragg@gmail.com>
2022-10-09 15:21:16 -0400

    Added me fetch request and began routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Login from "./Login";
+import Signup from "./Signup";
+import Albums from "./Albums";
+// import NavBar from "./NavBar";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  //For keeping user logged in
+  useEffect(() => {
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        console.log(r)
+        r.json().then((user) => setUser(user));
+      }
+    });
+
+  }, []);
+
+  if (!user) return <Login onLogin={setUser} />
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+            <li>
+              <Link to="/albums">Albums</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/albums" element={<Albums />} />
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
