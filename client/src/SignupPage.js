85e9e1b7a936ff571bc6b2b331501af6c06eeb71
Emily <emilybribragg@gmail.com>
2022-10-14 11:12:47 -0400

    Added POST request errors

---
+++ client/src/SignupPage.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import Error from "./styles/Error.js";
+import FormField from "./styles/FormField.js";
+import Button from "./styles/Button.js";
+import Input from "./styles/Input.js";
+import Label from "./styles/Label.js";
+
+function SignupPage( { onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [errors, setErrors] = useState([]);
+
+ //POST request for creating a new user
+ function handleSubmit(e) {
+  e.preventDefault();
+  setErrors([]);
+  fetch("/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+      password_confirmation: passwordConfirmation,
+    }),
+  }).then((r) => {
+    if (r.ok) {
+      r.json().then((user) => onLogin(user));
+      console.log("you are logged in")
+    } else {
+      r.json().then((err) => setErrors(err.errors));
+    }
+  });
+}
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormField>
+        <Label htmlFor="username">Username</Label>
+        <Input type="text" id="username" autoComplete="off" value={username} onChange={(e) => setUsername(e.target.value)}/>
+      </FormField>
+
+      <FormField>
+        <Label htmlFor="password">Password</Label>
+        <Input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} autoComplete="current-password"/>
+      </FormField>
+
+      <FormField>
+        <Label htmlFor="password">Password Confirmation</Label>
+        <Input type="password" id="password_confirmation" value={passwordConfirmation} onChange={(e) => setPasswordConfirmation(e.target.value)} autoComplete="current-password"/>
+      </FormField>
+      
+      <FormField>
+        <Button type="submit">Sign Up</Button>
+      </FormField>
+
+      <FormField>
+        {errors.map((err) => (
+          <Error key={err}>{err}</Error>
+        ))}
+      </FormField>
+    </form>
+    
+  );
+}
+
+export default SignupPage;
