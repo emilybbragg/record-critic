4ad5f2a69ff7a248be201720703fb99a92bf76f3
Emily <emilybribragg@gmail.com>
2022-10-14 11:08:19 -0400

    Added attributes

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,4 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :rating, :album_id, :user_id
+  attributes :id, :title, :description, :rating
+#   # , :album_id, :user_id
 end
