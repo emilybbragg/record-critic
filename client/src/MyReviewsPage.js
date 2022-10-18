23a6c5c5b7554e37d4c3524a7838f510e0c10db4
Emily <emilybribragg@gmail.com>
2022-10-18 18:25:57 -0400

    Created my reviews page

---
+++ client/src/MyReviewsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MyReviewsPage() {
+
+
+  //render all the albums and their corresponding reviews that the logged in user has left
+
+  return (
+    <div>Here are this user's reviews</div>
+  )
+}
+
+export default MyReviewsPage;
