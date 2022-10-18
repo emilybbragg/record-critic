c524f6d22b52ca81d102a3b9c0a1ead1509e2fea
Emily <emilybribragg@gmail.com>
2022-10-18 18:28:06 -0400

    Added show route/fixed destroy route

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,51 +1,47 @@
 class ReviewsController < ApplicationController
 
-  # def index 
-  #   reviews = Review.all
-  #   render json: reviews
-  # end
-
-  # def show 
-  #   review = review
-  #   render json: review
-  # end
-
-  # def create 
-  #   review = Review.create!(review)
-  #   render json: review, status: :created
-  # end
-
-
   def index
     if params[:album_id]
       album = Album.find(params[:album_id])
       reviews = album.reviews
-      render json: reviews, include: :album
+      render json: reviews
+      # , include: :album
     else
       render json: { errors: ["No reviews yet"] }
     end
   end
 
-  # def index
-  #   render json: Review.all
-  # end
+  def show
+    review = Review.find(params[:id])
+    render json: review
+  end
 
   def create
     review = Review.create(review_params)
     render json: review, status: :created
   end
 
-  # def update
-  #   review = find_review
-  #   review.update!(review_params)
-  #   render json: review
-  # end
+  def update
+    review = find_review
+    review.update!(review_params)
+    render json: review
+  end
 
   # def destroy 
   #   review = find_review
   #   review.destroy
   #   head :no_content
   # end
+
+  def destroy
+    review = Review.find_by(id: params[:id])
+    if review
+      review.destroy
+      head :no_content
+    else
+      render json: { error: "Review not found" }, status: :not_found
+    end
+  end
 
   private
 
@@ -54,10 +50,7 @@ class ReviewsController < ApplicationController
   end
 
   def review_params
-      # params.permit(:title, :description, :rating, :user_id, :album_id)
-      params.permit(:title, :description, :rating)
-
+    params.permit(:title, :description, :rating, :album_id, :user_id)
   end
-
 
 end
