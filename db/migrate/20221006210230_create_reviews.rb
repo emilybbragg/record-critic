a832d176e8404506f73137b4d06d98f0ba3a61a3
Emily <emilybribragg@gmail.com>
2022-10-25 20:52:18 -0400

    Added foreign key

---
+++ db/migrate/20221006210230_create_reviews.rb
@@ -0,0 +1,13 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reviews do |t|
+      t.string :title
+      t.string :description
+      t.integer :rating
+      t.belongs_to :album, index: true, foreign_key: true
+      t.belongs_to :user, index: true, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
