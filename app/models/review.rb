4005d1b9b4aa1f81dda9fee92cbed6db665eeab2
Emily <emilybribragg@gmail.com>
2022-10-25 20:51:03 -0400

    Added validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,9 @@ class Review < ApplicationRecord
 
   belongs_to :user
   belongs_to :album
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true, length: { is: 1 }
   
 end
