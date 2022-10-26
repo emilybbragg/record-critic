e1b1c21bec5fb94783f556aaccbd67e21c1a7a10
Emily <emilybribragg@gmail.com>
2022-10-25 20:48:22 -0400

    Created reviewlist component to separate reviews from albums

---
+++ client/src/ReviewList.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react';
+import FormField from "./styles/FormField.js";
+import Input from "./styles/Input.js";
+import Label from "./styles/Label.js";
+import styled from "styled-components";
+import Button from "./styles/Button.js";
+import Review from "./Review";
+
+function ReviewList( {user, albumId, reviews, setReviews, onUpdateReview}) {
+
+  const [reviewTitle, setReviewTitle] = useState("");
+  const [reviewDescription, setReviewDescription] = useState("");
+  const [reviewRating, setReviewRating] = useState("");
+
+  function handleReviewSubmit(e) {
+    e.preventDefault();
+    const reviewData = {
+      title: reviewTitle,
+      description: reviewDescription,
+      rating: reviewRating,
+      user_id: user.id,
+      album_id: parseInt(albumId),
+    };
+    fetch(`/reviews`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+        body: JSON.stringify(reviewData),
+    })
+      .then((r) => r.json())
+      .then((newReview) => {
+        const allReviewsWithNew = [...reviews, newReview]
+        setReviews(allReviewsWithNew);
+        setReviewTitle("");
+        setReviewDescription("");
+        setReviewRating("");
+      })
+  }
+
+  function handleReviewDeleteClick(review) {
+    fetch(`/reviews/${review.id}`, {
+        method: "DELETE",
+    })
+    .then((r) => {
+      if (r.ok) {
+        deleteReview(review)
+      }
+    })
+  }
+  
+  function deleteReview(deletedReview) {
+    const updatedReviews = reviews.filter((review) => review.id !== deletedReview.id)
+    setReviews(updatedReviews)
+  }
+
+  function handleUpdateReview(updatedReview) {
+    const editedReviews = reviews.map((review) => {
+      if (review.id === updatedReview.id) {
+        return updatedReview;
+      } else {
+        return review;
+      }
+    });
+    setReviews(editedReviews);
+  }
+
+  return (
+    <>
+      <Wrapper>
+      <div className="review-container">
+        {reviews?.length > 0 ? (reviews.map((review) => (
+          <Review key={review.id} id={review.id} review={review} handleReviewDeleteClick={handleReviewDeleteClick}  handleUpdateReview={handleUpdateReview}/>
+          ))
+        ) : null}
+      
+          <form className="reviewForm" onSubmit={handleReviewSubmit}>
+            <div className="writeReview">Leave a Review:</div>
+            <br></br>
+            <FormField>
+              <Label htmlFor="titleInput">Title:</Label>
+              <Input type="text" id="title" value={reviewTitle} onChange={(e) => setReviewTitle(e.target.value)}/>
+            </FormField>
+
+            <FormField>
+              <Label htmlFor="descInput">Review:</Label>
+              <Input type="text" id="desc" value={reviewDescription} onChange={(e) => setReviewDescription(e.target.value)}/>
+            </FormField>
+
+            <FormField>
+              <Label htmlFor="ratingInput">Rating (Out of 5):</Label>
+              <Input type="text" id="rating" value={reviewRating} onChange={(e) => setReviewRating(e.target.value)}/>
+            </FormField>
+
+            <FormField>
+              <Button type="submit">Submit</Button>
+            </FormField>
+          </form>
+        </div>
+      </Wrapper>
+    </>
+  );
+}
+
+const Wrapper = styled.section`
+  max-width: 800px;
+  margin: 40px auto;
+`;
+
+export default ReviewList;
+
+
+//other way of mapping reviews - probably not necessary
+  // const allReviews = reviews.map((review) => {
+  //   return <Review key={review.id} review={review} title={review.title} description={review.description} rating={review.rating} handleReviewDeleteClick={handleReviewDeleteClick} />
+  // });
