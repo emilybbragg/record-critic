4443ffc88dc956ddce25b498e8432a7f0dafcd3e
Emily <emilybribragg@gmail.com>
2022-10-25 20:51:14 -0400

    Added validations

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,4 +3,9 @@ class Album < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
 
+  validates :name, presence: true, uniqueness: true
+  validates :artist, presence: true
+  validates :year, presence: true, length: { is: 4 }
+  validates :image, presence: true
+
 end
