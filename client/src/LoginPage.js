a5dc892b72275ea986c6f2b71137d377574d9963
Emily <emilybribragg@gmail.com>
2022-10-26 21:24:17 -0400

    Added isloading feature

--- client/src/LoginPage.js
+++ client/src/LoginPage.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import SignupPage from "./SignupPage";
 import LoginForm from "./LoginForm";
 import Button from "./styles/Button.js";
 import styled from "styled-components";
 
-function LoginPage({ onLogin }) {
+function LoginPage({ onLogin, user }) {
+  const navigate = useNavigate()
   const [showLogin, setShowLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(true)
+
+
+  console.log("USER FROM LOGIN PAGE")
+  console.log(user)
+
+  useEffect(() => {
+    if (user) {
+      navigate("/albums")
+    } else {
+      setIsLoading(false)
+    }
+  }, [user])
+  
 
   return (
+    <>
+    {isLoading ? <div>Loading our user....</div> :
     <Wrapper>
       <div className="login-form">
         <h1 className="title">Record Critic</h1>
@@ -33,7 +51,8 @@ function LoginPage({ onLogin }) {
           </>
         )}
       </div>
-    </Wrapper>
+    </Wrapper>}
+    </>
   );
 }
 
