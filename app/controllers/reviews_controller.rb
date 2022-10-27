95c9a354b9a400a94f1a095b267dc23ac6944c28
Emily <emilybribragg@gmail.com>
2022-10-26 21:25:43 -0400

    Debugging

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,19 +10,27 @@ class ReviewsController < ApplicationController
     end
   end
 
+  # def index
+  #   if params[:user_id]
+  #     user = User.find_by(params[:user_id])
+  #     reviews = user.reviews
+  #     render json: reviews
+  #   else
+  #     render json: { errors: ["No reviews yet"] }
+  #   end
+  # end
+
   def show
     review = Review.find(params[:id])
     render json: review
   end
 
   def create
-    # review = Review.create!(review_params)
     review = @current_user.reviews.create!(review_params)
     render json: review, status: :created
   end
 
   def update
-    # return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
     review = Review.find_by(id: params[:id])
     review.update!(review_params)
     # review = @current_user.reviews.update!(review_params)
@@ -30,7 +38,6 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
-    # review = Review.find_by(id: params[:id])
     review = find_review
     if review
       review.destroy
