3ce3dfddc50e48f1c91c37d00f2ec43ab2957d9c
Emily <emilybribragg@gmail.com>
2022-10-26 21:23:32 -0400

    Fixed issue with user editing

--- client/src/Review.js
+++ client/src/Review.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import EditReview from "./EditReview";
 
-function Review ( {handleReviewDeleteClick, review, handleUpdateReview, user} ) {
+function Review ( {handleReviewDeleteClick, review, handleUpdateReview, user, reviews, setReviews} ) {
 
-  const [updatedDescription, setUpdatedDescription] = useState("");
   const [isEditing, setIsEditing] = useState(false);
 
   return (
@@ -12,29 +11,31 @@ function Review ( {handleReviewDeleteClick, review, handleUpdateReview, user} ) 
       <ul className="reviewsList">
         <span className="review-title">{review?.title}</span>
         Rating: {review?.rating}/5
-        <span>{review?.description}</span>
+        <br></br>
+        {review?.description}
         <p> - {review?.user?.username}</p>
       
-       {isEditing ? (
+       {isEditing && review.user_id == user.id ? (
         <EditReview
           id={review.id}
           review={review}
           setIsEditing={setIsEditing}
           handleUpdateReview={handleUpdateReview}
+          user={user.id}
         />
    
-       ) : (
+       ) : review.user_id == user.id ? (
         <div className="review-buttons">
-           <button className="editButton" onClick={() => setIsEditing((isEditing) => !isEditing)}>
+          <button className="editButton" onClick={() => setIsEditing((isEditing) => !isEditing)}>
             <span role="img" aria-label="edit">Edit ✏️</span>
           </button>
           <button className="deleteButton" onClick={() => handleReviewDeleteClick(review)}>
             <span role="img" aria-label="delete">Delete 🗑</span>
           </button>
-    
-         
         </div>
-        )}
+        )
+        : null}
+
       </ul>
     </div>
     </>
