dea1fbdd8e8737e94456101aa16fd93de523aede
Emily <emilybribragg@gmail.com>
2022-10-26 21:23:53 -0400

    Started reviews page for individual users

--- client/src/MyReviewsPage.js
+++ client/src/MyReviewsPage.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { useParams } from "react-router-dom";
+import Review from "./Review";
 
-function MyReviewsPage() {
+function MyReviewsPage( {reviews, review, setReviews, user} ) {
+
+  const [userReviews, setUserReviews] = useState([]);
+
+  function displayUserReviews(user) {
+    const userReviews = reviews.filter((review) => review.id !== user.id)
+    setReviews(userReviews)
+  }
+
+
+  const {userId} = useParams();
+
+  useEffect(() => {
+    console.log(userReviews)
+    fetch("/user/${userId}/reviews")
+      .then((r) => r.json())
+      .then(userReviews => setUserReviews(userReviews))
+  }, [userId])
+
 
 
   //render all the albums and their corresponding reviews that the logged in user has left
 
   return (
-    <div>Here are this user's reviews</div>
+    <div>
+    <ul>
+      {/* <Review
+      userId={userId}
+      userReviews={userReviews}
+      displayUserReviews = {displayUserReviews}
+      /> */}
+    {/* {displayUserReviews} */}
+    
+    </ul>
+      
+    </div>
   )
 }
 
