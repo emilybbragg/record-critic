72fd54bcbf30063fb92a694fb9889898d4ccf3a7
Emily <emilybribragg@gmail.com>
2022-10-28 17:12:45 -0400

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :albums, through: :reviews
 
-  # validates :username, presence: true, uniqueness: true
-  # validates :password, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, uniqueness: true
 
   
 end
