032d7b43cbfcb6ca4658769775f5aa73ec4d2b0d
Emily <emilybribragg@gmail.com>
2022-11-07 17:28:43 -0500

    Added distinct feature for associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   has_secure_password
   has_many :reviews
-  has_many :albums, through: :reviews
+  has_many :albums, -> { distinct }, through: :reviews
 
   validates :username, presence: true, uniqueness: true
   
