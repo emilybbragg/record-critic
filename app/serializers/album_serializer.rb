15674734be8b3f8ddb7118620d9cc9dfe4795169
Emily <emilybribragg@gmail.com>
2022-11-21 19:22:12 -0500

    Added custom serializer

--- app/serializers/album_serializer.rb
+++ app/serializers/album_serializer.rb
@@ -1,5 +1,22 @@
 class AlbumSerializer < ActiveModel::Serializer
-  attributes :id, :name, :artist, :image, :year
+  attributes :id, :name, :artist, :image, :year, :album_reviews
 
   has_many :reviews
+  has_many :users, through: :reviews
+
+  def album_reviews
+    object.reviews.map do |review| 
+      {
+        id: review.id,
+        title: review.title,
+        description: review.description,
+        rating: review.rating,
+        username: review.user.username,
+        user_id: review.user.id,
+        album: review.album.name,
+        album_id: review.album.id,
+        artist: review.album.artist
+      }
+    end
+  end
 end
