58941fe2a8658a6e2a59d1d74ab5187e6149325c
Emily <emilybribragg@gmail.com>
2022-11-21 19:25:39 -0500

    Cleaned up error setting

--- client/src/SignupPage.js
+++ client/src/SignupPage.js
@@ -11,34 +11,29 @@ function SignupPage( { onLogin }) {
   const [errors, setErrors] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
- function handleSubmit(e) {
-  e.preventDefault();
-  setIsLoading(true);
-  setErrors([]);
-  fetch("/signup", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username,
-      password,
-      password_confirmation: passwordConfirmation
-    }),
-  }).then((r) => {
-    setIsLoading(false);
-    if (r.ok) {
-      r.json().then((user) => onLogin(user));
-    // } else {
-    //   r.json().then((err) => {
-    //     setErrors([err.error]);
-    //   })
-  } else {
-    r.json().then((err) => setErrors(err.errors))
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsLoading(true);
+    setErrors([]);
+    fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        password_confirmation: passwordConfirmation
+      }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json().then((user) => onLogin(user));
+    } else {
+      r.json().then((err) => setErrors(err.errors))
+    } 
+    });
   }
-    
-  });
-}
 
   return (
     <form onSubmit={handleSubmit}>
