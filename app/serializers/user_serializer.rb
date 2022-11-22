ebaf251b4a1313a7fd55cd40a594b51635a4bf57
Emily <emilybribragg@gmail.com>
2022-11-21 19:23:12 -0500

    Added custom serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,23 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username
+  attributes :id, :username, :reviews
 
   has_many :reviews
+  has_many :albums, through: :reviews
+
+  def reviews
+    object.reviews.map do |review| 
+      {
+        id: review.id,
+        title: review.title,
+        description: review.description,
+        rating: review.rating,
+        username: review.user.username,
+        user_id: review.user.id,
+        album: review.album,
+        album_id: review.album.id,
+        artist: review.album.artist
+      }
+    end
+  end
+
 end
